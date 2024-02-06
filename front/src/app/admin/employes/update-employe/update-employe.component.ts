40a065f0d21e21e0f3efbed4b3c57d158e1719ee
Jovani Matthieu <ravelonarivojovani@gmail.com>
2024-02-06 21:33:19 +0300

    Ajout de front

---
+++ front/src/app/admin/employes/update-employe/update-employe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-employe',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-employe.component.html',
+  styleUrl: './update-employe.component.css'
+})
+export class UpdateEmployeComponent {
+
+}
---
+++ front/src/app/admin/services/create-service/create-service.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-service',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-service.component.html',
+  styleUrl: './create-service.component.css'
+})
+export class CreateServiceComponent {
+
+}

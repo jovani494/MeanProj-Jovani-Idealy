6b1990b8e175761134fd11d8feadba768a899e83
Jovani Matthieu <ravelonarivojovani@gmail.com>
2024-02-12 22:24:04 +0300

    Service front

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ServicelistComponent } from './admin/service/servicelist/servicelist.component';
 import { ServicecreateComponent } from './admin/service/servicecreate/servicecreate.component';
-
+import { ServicedetailsComponent } from './admin/service/servicedetails/servicedetails.component';
 
 const routes: Routes = [
   {path: 'admin/service', component : ServicelistComponent},
-  {path: 'admin/service/create', component : ServicecreateComponent}
+  {path: 'admin/service/create', component : ServicecreateComponent},
+  {path: 'admin/service/:id', component : ServicedetailsComponent}
 ];
 
 @NgModule({
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HttpClientModule} from '@angular/common/http';
+import { ServiceService } from './admin/service/service.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServicelistComponent } from './admin/service/servicelist/servicelist.component';
 import { ServicecreateComponent } from './admin/service/servicecreate/servicecreate.component';
+import { ServicedetailsComponent } from './admin/service/servicedetails/servicedetails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServicelistComponent,
-    ServicecreateComponent
+    ServicecreateComponent,
+    ServicedetailsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

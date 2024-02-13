f422743568c8540b9397952a8143ae072e006006
ideaaly <idealy175@gmail.com>
2024-02-13 11:22:01 +0300

    modified:   src/app/admin/employes/create-employe/create-employe.component.html         modified:   src/app/admin/employes/create-employe/create-employe.component.ts         modified:   src/app/admin/services/services.service.ts         modified:   src/app/app.component.ts         modified:   src/app/app.routes.ts         new file:   src/app/client/client.component.css         new file:   src/app/client/client.component.html         new file:   src/app/client/client.component.spec.ts         new file:   src/app/client/client.component.ts         new file:   src/app/home/home.component.css         new file:   src/app/home/home.component.html         new file:   src/app/home/home.component.spec.ts         new file:   src/app/home/home.component.ts

--- front/src/app/admin/services/services.service.ts
+++ front/src/app/admin/services/services.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // Définition des méthodes du service ici
 }
